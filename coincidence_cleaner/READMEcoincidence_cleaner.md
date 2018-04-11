8a8b26817e8cd8ac62e064794d25dace60484b6d
sven314 <sven.peter@physik.uni-giessen.de>
2018-04-11 12:19:10 +0000

    output auf Konsole vereinfacht, Kommentare ergaenzt

--- coincidence_cleaner/READMEcoincidence_cleaner.md
+++ coincidence_cleaner/READMEcoincidence_cleaner.md
@@ -1,7 +1,8 @@
-Bereinigt output von compare oder other_compare von solchen Situationen: (1 1,5 2) und (1,5 2) gefunden. Beachtet den unwahrschjeinlichen Fall, dass Treffer identisch scheinen aber nicht sind, das sie von verschiedenen Stationen kommen
+Bereinigt output von compare oder other_compare von solchen Situationen: (1 1,5 2) und (1,5 2) gefunden. Beachtet den unwahrscheinlichen Fall, 
+dass Treffer identisch scheinen, aber nicht sind, da sie von verschiedenen Stationen kommen
 
 
-Beispiel fuer erwartete Eingabe: 
+Beispiel fuer erwartete Eingabe mit Stationsnummer und Timestamp:
 4 502047694.000000000 5 502047695.000000000 6 502047696.000000000
 1 502047692.000000000 2 502047694.000000000
 2 502047691.000000000 1 502047692.000000000 
@@ -10,4 +11,12 @@ Beispiel fuer erwartete Eingabe:
 4 502047690.000000000 5 502047691.000000000 6 502047692.000000000
 
 Treffer muessen nach dem ersten Timestamp Absteigend sortiert sein. (Das wird noch nicht geprueft oder korrigiert.)
-Innere Treffer stehen dann vor dem goesseren, da der Algorithmus das so ausspuckt.
+Innere Treffer stehen dann vor dem aeusseren, da der Algorithmus das so ausspuckt.
+
+Es ist noch notwendig, dass im aeusseren Treffer kein Event enthalten ist, das nach den letzten des inneren Treffers kommt. Beispiel:
+
+5 502047691.000000000 6 502047692.000000000 wird nicht in
+4 502047690.000000000 5 502047691.000000000 6 502047692.000000000 7 502047693.000000000 gefunden. 
+
+Das Problem ist gering, da solche Situationen vom Algorithmus nicht erzeugt werden.
+ 
--- coincidence_cleaner/coincidence_cleaner.cpp
+++ coincidence_cleaner/coincidence_cleaner.cpp
@@ -17,35 +17,6 @@
 #include <limits.h>		//manche unnoetig
 
 
-#include <vector>
-#include <fstream>
-#include <iostream>
-#include <sstream>
-#include <string>
-#include <stdio.h>
-#include <stdlib.h>
-#include <limits>
-#include <limits.h>
-
-//Hiervon sind mache unnoetig
-#include <fstream>
-#include <iostream>
-#include <sstream>
-#include <string>
-#include <vector>
-#include <stdio.h>
-#include <math.h>
-#include <time.h>
-#include <iomanip>
-#include <stdlib.h>
-#include <limits>
-#include <unistd.h>
-#include <functional>
-#include <fstream>
-#include <omp.h>
-#include <utility>
-#include <limits.h>
-
 using namespace std;
 
 void Usage()
@@ -56,42 +27,45 @@ struct Event {
 	unsigned int station;
 	
 	};
-
-bool included(vector<vector<Event> > allCoincidents, int tested, int compared, bool verbose){		//tested: das was potentiell enthalten ist
+//Prueft, ob tested in compared enthalten ist
+bool included(vector<vector<Event> > allCoincidents, int tested, int compared, bool verbose){	
 	if(verbose){cout<<"included gestartet"<<endl; }
 	
 	
 	
 	int testedLength=allCoincidents[tested].size(), comparedLength=allCoincidents[compared].size();
-	cout<<"testedLength "<<testedLength<<"ComparedLength "<<comparedLength<<endl;
+	if(verbose){cout<<"testedLength "<<testedLength<<"ComparedLength "<<comparedLength<<endl;}
 	//if(comparedLength<testedLength){return false;}		//Wenn es zu lang ist, passt es nicht rein
 	
 	for(int k=1; testedLength-k>=0&&comparedLength-k>=0;k++){
 		
 	
 		
-		cout<<"vergleiche "<<allCoincidents[tested][testedLength-k].station<<" "<<allCoincidents[tested][testedLength-k].timestamp<<" mit "<<allCoincidents[compared][comparedLength-k].station<<" "<<allCoincidents[compared][comparedLength-k].timestamp<<endl;
-		cout<<"k: "<<k<<endl;
+		if (verbose){
+			cout<<"vergleiche "<<allCoincidents[tested][testedLength-k].station<<" "<<allCoincidents[tested][testedLength-k].timestamp<<" mit "<<allCoincidents[compared][comparedLength-k].station<<" "<<allCoincidents[compared][comparedLength-k].timestamp<<endl;
+			cout<<"k: "<<k<<endl;
+			}
 	
 		if(allCoincidents[tested][testedLength-k].station!=allCoincidents[compared][comparedLength-k].station){
 			
-			//if(verbose){cout<<"Stationen "<<allCoincidents[tested][testedLength-k].station<<" "<<allCoincidents[compared][comparedLength-k].station<<endl;}
 			
-			cout<<"stationenUnterscghied"<<endl;
+			
+			if(verbose){cout<<"stationenUnterschied"<<endl;}
 			return false;
 			
+			}		//Wenn sie sich irgendwo unterscheiden, muss man nicht weiter pruefen, also return
 			
 			
-			}		//Wenn sie sich irgendwo unterscheiden, muss man nicht weiter prüfen
+			
 		if(allCoincidents[tested][testedLength-k].timestamp-allCoincidents[compared][comparedLength-k].timestamp>0.1){
-			cout<<"timestampUnterschied"<<endl;
-			//if(verbose){cout<<"Timestamp Differenz "<<allCoincidents[tested][testedLength-k].timestamp-allCoincidents[compared][comparedLength-k].timestamp<<endl;}
+			if(verbose){cout<<"timestampUnterschied"<<endl;}
+			
 			
 			return false;
 			}
 		
 	
-		cout<<"Kein Unterschied gefunden"<<endl;
+		if(verbose){cout<<"Kein Unterschied gefunden"<<endl;}
 		
 		
 		}
@@ -118,13 +92,13 @@ int main(int argc, char*argv[])
 	bool verbose = false;	
 	int ch;
 
-	int b = 100;
+	
 
 	
 	cout << fixed << setprecision(9);		//gibt double auf 9 Nachkommastellen aus
 	
 	//Lesen der Argumente von der Kommandozeile
-	while ((ch = getopt(argc, argv, "svm:c:o:b:h?"))!=EOF)
+	while ((ch = getopt(argc, argv, "svm:c:o:h?"))!=EOF)
 	{
 		switch ((char)ch)
 		{
@@ -135,11 +109,10 @@ int main(int argc, char*argv[])
 			break;
 		case 'h': Usage();
 			return -1;
-			return -1;
+
 			break;
 		
-		case 'b': b = atoi(optarg);
-			break;
+	
 
 	
 		case 'm': maxCoincidentsAtOnce = atoi(optarg);
@@ -169,10 +142,10 @@ int main(int argc, char*argv[])
 
 	ifstream dateiInputstream(dateiName[0].c_str());
 	string oneLine;
-	vector<Event >  oneCoincidence;	
-	vector<vector<Event> > allCoincidents;
+	vector<Event>  oneCoincidence;			//Eine kKoinzidenz ist eine Vector von Events
+	vector<vector<Event> > allCoincidents;	//	Alle Koinzidenzen als Vektor aus Vektoren aus Events
 	
-	cout<<"z"<<endl;
+	//Lies
 	for (unsigned int i = 0; i<maxCoincidentsAtOnce; i++)	{
 		if(verbose) {cout<<"neue Zeile"<<endl;}
 		if (dateiInputstream.eof())//Wenn fertig
@@ -186,7 +159,7 @@ int main(int argc, char*argv[])
 		if(oneLine.empty()){continue;}		//ignore empty lines
 	
 	
-		
+			//Suche Spaces zum Zerschneiden der Zeilen
 			int space0=0;
 			int space1=oneLine.find(" ",space0+1);
 			int space2=oneLine.find(" ",space1+1);
@@ -194,6 +167,8 @@ int main(int argc, char*argv[])
 			int oneStation, oneTimestamp;
 			
 			int counter=0;
+			
+		//Liest eine Zeile
 		while(true)	{
 	
 			
@@ -212,19 +187,19 @@ int main(int argc, char*argv[])
 			str2>>oneTimestamp;
 			newEvent.timestamp=oneTimestamp;
 			
-			cout<<newEvent.station<<" ";
+			if(verbose){cout<<newEvent.station<<" ";}
 			
 	
 			oneCoincidence.push_back(newEvent);
 			
-			cout<<oneCoincidence[counter].station<<endl;
+			if(verbose){cout<<oneCoincidence[counter].station<<endl;}
 			//Schiebe die spaces 0--2, 1 hinter 2 und 2 hinter neue 1
 			space0=space2;
 			space1=oneLine.find(" ",space2+1);
 			space2=oneLine.find(" ",space1+1);
 			counter++;}
 			
-		
+			//Wenn eine Zeile zu Ende ist, gibt es eine Exception.
 			catch (exception E){
 				if(verbose){cout<<"caught out of range"<<endl;}
 				break;
@@ -234,13 +209,6 @@ int main(int argc, char*argv[])
 		 allCoincidents.push_back(oneCoincidence);
 	}
 	
-	
-			//hoechstens maxValues werden gelesen //Auslagern in eigene Funktion
-	
-	
-		
-
-
 
 	
 	//Ausgabe um Lesen zu pruefen
@@ -248,51 +216,51 @@ int main(int argc, char*argv[])
 	cout<<"Zeilenzahl: "<<allCoincidents.size()<<endl;
 	if(verbose){cout<<"Ausgabe beginnt"<<endl;}
 	for (int i=0;i<allCoincidents.size();i++){		// j innerhalb einer Coincidednmce, i fuer die Zeile  
-		//if(verbose){cout<<"neue Zeile beginnt"<<endl;}
+		
 		for(int j=0; j<allCoincidents[i].size(); j++){
-			//cout<<"i: "<<i<<" j: "<<j<<"  ";
-			cout<<allCoincidents[i][j].station<<": ";
-			cout<<allCoincidents[i][j].timestamp<<"    ";
+			
+			if(verbose){
+				
+				cout<<allCoincidents[i][j].station<<": ";
+				cout<<allCoincidents[i][j].timestamp<<"    ";}
 			
 		}
 		
-		cout<<endl;
+		if(verbose){cout<<endl;}
 	}
 	bool includedCoincidents[allCoincidents.size()];
 	
-	for(int i=0; i<allCoincidents.size();i++){
-		includedCoincidents[i]=false;
-		
-		
+	for(int i=0; i<allCoincidents.size();i++){		//in includedCoincidents  wird vermerkt, ob eine Koinzidenz in einer anderen enthalten ist.
+		includedCoincidents[i]=false;	//initialisiere mit false
 		}
 	
 	for(int i=0;i<allCoincidents.size();i++) {
-		cout<<"i: "<<i<<endl;
-		for(int j=i+1; j<allCoincidents.size();j++){		//man muss nur bis zum Ende der Liste suchen und faengt an, was man ueberprueft
+		if(verbose){cout<<"i: "<<i<<endl;}
+		for(int j=i+1; j<allCoincidents.size();j++){		//man muss nur maximal bis zum Ende der Liste suchen und faengt eine Zeile unter dem zu Pruefenden an
 			
 			if(included(allCoincidents, i, j, verbose)){
-			includedCoincidents[i]=true;
+			includedCoincidents[i]=true;		
 			
-			cout<<"enthalten"<<endl;
+			if(verbose){cout<<"enthalten"<<endl;}
 			break;
 			}
 				
 				
-			cout<<"j: "<<j<<" "<<endl;;
+			if(verbose){cout<<"j: "<<j<<" "<<endl;}
 		
-			if(allCoincidents[i][0].timestamp-allCoincidents[j][allCoincidents[j].size()-2].timestamp>match){
-				break;}		//Suche nicht mehr weiter, wenn die Abstaende zu gross werden
+			if(allCoincidents[i][0].timestamp-allCoincidents[j][0].timestamp>match){
+				break;}		//Suche nicht mehr weiter, wenn die Zeit-Abstaende zu gross werden
 			
 			
 			
 			}
+			
 		
-		cout<<endl;
+		if(verbose){cout<<endl;}
 		}
 	cout<<"Ergebnis"<<endl;	
 	
-	cout<<"true "<<true<<endl;
-	cout<<"false "<<false<<endl;
+
 		
 	ofstream output(outputDateiName.c_str());
 	
@@ -300,23 +268,31 @@ int main(int argc, char*argv[])
 	
 	
 	int discarded=0;
+	
+	//Zusammenfassung auf Konsole schreiben
 	for(int i=0; i<allCoincidents.size();i++){
-		
+		cout<<"Zeile: "<<i;
 		if(includedCoincidents[i]){
 			discarded++;
+			cout<<" verworfen"<<endl;
 			
 			
-			}
+			
+			}else{
+				cout<<" behalten"<<endl;}
 		
 		}
 	
-		
+	cout<<discarded<<" Zeilen verworfen"<<endl;
+	
+	
+	//Gefilterte Daten in outputfile schreiben.	
 	for (int i=0;i<allCoincidents.size();i++){		// j innerhalb einer Coincidednmce, i fuer die Zeile  
-		//if(verbose){cout<<"neue Zeile beginnt"<<endl;}
+		
 		if(!includedCoincidents[i]){
 		
 		for(int j=0; j<allCoincidents[i].size(); j++){
-			//cout<<"i: "<<i<<" j: "<<j<<"  ";
+	
 			
 			output<<allCoincidents[i][j].station<<": ";
 			output<<allCoincidents[i][j].timestamp<<"    ";}
@@ -327,10 +303,7 @@ int main(int argc, char*argv[])
 		
 		}
 	}
-		
-		
-		cout<<discarded<<" Zeilen verworfen"<<endl;
-		}
+}
 	
 
 	
